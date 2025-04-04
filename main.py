e26f3496c893c2502f46cd803ff03971e7508f00
Leilani <158503738+leidoe07@users.noreply.github.com>
2025-04-04 08:55:48 -0400

    Linking swiper to filters (#22)
    * Add ingredients display and saved recipes link; update navbar and recipe detail page
    
    * name
    
    * Remove Korean cuisine description from homepage card
    
    * Update recipe pages and UI elements; add login prompts and change Korean to British recipes
    
    ---------
    
    Co-authored-by: ramatoulay123 <rbarry@brooklynsteamcenter.org>

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def connect_db ():
     conn = pymysql.connect(            
         host= "db.steamcenter.tech",
         database= "pantryfy",
-        user = "spowell", 
+        user = "ldore",  
         password = conf.password, 
         autocommit= True,   
         cursorclass= pymysql.cursors.DictCursor, 
@@ -149,9 +149,18 @@ def recipe_detail(recipe_id):
     recipe = cursor.fetchone()    
 
 
+    cursor.execute(f""" SELECT * FROM 	`RecipeIngredients` JOIN `Recipe` ON`Recipe`.`id`= `RecipeIngredients`.`recipe_id`
+                    JOIN `Ingredients` ON `Ingredients`.`id` = `RecipeIngredients`.`ingredient_id`
+                    WHERE `recipe_id` = {recipe_id}
+                   ;""")
+    ingredients = cursor.fetchall()
+
+    
+
     cursor.execute(f""" 
         SELECT * FROM Review WHERE `recipe_id` = {recipe_id};
     """)  
+
     reviews = cursor.fetchall()          
 
 
@@ -192,7 +201,7 @@ def recipe_detail(recipe_id):
     print(recipe) 
 
 
-    return render_template("individual_recipe.html.jinja", recipe = recipe, reviews = reviews) 
+    return render_template("individual_recipe.html.jinja", recipe = recipe, reviews = reviews, ingredients = ingredients) 
     
 
 
@@ -308,7 +317,9 @@ def search_page():
 def catolog_page():
     
     if flask_login.current_user.is_authenticated == False:
+        flash("Please log in to view your ingredients.")
         return redirect("/signin")
+        
     
     conn = connect_db()
     cursor = conn.cursor()
@@ -360,6 +371,8 @@ def add_ingredient_page():
         conn.close
     else:
         redirect("/login")
+        flash("Please log in to add ingredients.")
+        
 
 
         
@@ -381,11 +394,11 @@ def mexican_recipes():
     cursor.close()
     conn.close()    
     return render_template("mexican_recipes.html.jinja" , recipe = results)
-@app.route("/korean")
+@app.route("/british")
 def korean_recipes():
     conn = connect_db()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM `Recipe` WHERE `cuisine` = 'Korean';")
+    cursor.execute("SELECT * FROM `Recipe` WHERE `cuisine` = 'British';")
     results = cursor.fetchall()
     cursor.close()
     conn.close()    
